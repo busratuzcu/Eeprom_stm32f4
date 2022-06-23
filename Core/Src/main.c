093a13eb2c7ed05d6c0a981d45ff974729796e47
busratuzcu <busratuzcu@outlook.com>
2022-06-23 16:51:05 +0300

    led problem fixed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -41,9 +41,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint16_t VirtAddVarTab[NB_OF_VAR] = {0x5555, 0x6666, 0x7777};
-uint16_t VarDataTab[NB_OF_VAR] = {0, 0, 0};
-uint16_t VarValue,VarDataTmp = 0;
+uint16_t VirtAddVarTab[NB_OF_VAR] = { 0x5555, 0x6666, 0x7777 };
+uint16_t VarDataTab[NB_OF_VAR] = { 0, 0, 0 };
+uint16_t VarValue, VarDataTmp = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -59,265 +59,226 @@ static void MX_GPIO_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  HAL_FLASH_Unlock();
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	/* USER CODE BEGIN 2 */
 
-  /* EEPROM Init */
-  if( EE_Init() != EE_OK)
-  {
-    Error_Handler();
-  }
+	/* Unlock the Flash Program Erase controller */
+	HAL_FLASH_Unlock();
 
-  /* --- Store successively many values of the three variables in the EEPROM ---*/
-  /* Store 0x1000 values of Variable1 in EEPROM */
-  for (VarValue = 1; VarValue <= 0x2; VarValue++)
-  {
-    /* Sequence 1 */
-    if((EE_WriteVariable(VirtAddVarTab[0],  VarValue)) != HAL_OK)
-    {
-      Error_Handler();
-    }
-    if((EE_ReadVariable(VirtAddVarTab[0],  &VarDataTab[0])) != HAL_OK)
-    {
-      Error_Handler();
-    }
-    if (VarValue != VarDataTab[0])
-    {
-      Error_Handler();
-    }
-  }
-  HAL_FLASH_Lock();
+	/* EEPROM Init */
+	if (EE_Init() != EE_OK) {
+		Error_Handler();
+	}
+
+	/* --- Store successively many values of the three variables in the EEPROM ---*/
+	/* Store 0x1000 values of Variable1 in EEPROM */
+	for (VarValue = 1; VarValue <= 0x1; VarValue++) {
+		/* Sequence 1 */
+		if ((EE_WriteVariable(VirtAddVarTab[0], VarValue)) != HAL_OK) {
+			Error_Handler();
+		}
+		if ((EE_ReadVariable(VirtAddVarTab[0], &VarDataTab[0])) != HAL_OK) {
+			Error_Handler();
+		}
+		if (VarValue != VarDataTab[0]) {
+			Error_Handler();
+		}
+
+		/* Sequence 2 */
+		if (EE_WriteVariable(VirtAddVarTab[1], ~VarValue) != HAL_OK) {
+			Error_Handler();
+		}
+		if (EE_ReadVariable(VirtAddVarTab[1], &VarDataTab[1]) != HAL_OK) {
+			Error_Handler();
+		}
+		if (((uint16_t) ~VarValue) != VarDataTab[1]) {
+			Error_Handler();
+		}
+
+		/* Sequence 3 */
+		if (EE_WriteVariable(VirtAddVarTab[2], VarValue << 1) != HAL_OK) {
+			Error_Handler();
+		}
+		if (EE_ReadVariable(VirtAddVarTab[2], &VarDataTab[2]) != HAL_OK) {
+			Error_Handler();
+		}
+		if ((VarValue << 1) != VarDataTab[2]) {
+			Error_Handler();
+		}
+	}
 //
-//    /* Sequence 2 */
-//    if(EE_WriteVariable(VirtAddVarTab[1], ~VarValue) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(EE_ReadVariable(VirtAddVarTab[1], &VarDataTab[1]) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(((uint16_t)~VarValue) != VarDataTab[1])
-//    {
-//      Error_Handler();
-//    }
+//	/* Store 0x2000 values of Variable2 in EEPROM */
+//	for (VarValue = 1; VarValue <= 0x2; VarValue++) {
+//		if (EE_WriteVariable(VirtAddVarTab[1], VarValue) != HAL_OK) {
+//			Error_Handler();
+//		}
+//		if (EE_ReadVariable(VirtAddVarTab[1], &VarDataTab[1]) != HAL_OK) {
+//			Error_Handler();
+//		}
+//		if (VarValue != VarDataTab[1]) {
+//			Error_Handler();
+//		}
+//	}
 //
-//    /* Sequence 3 */
-//    if(EE_WriteVariable(VirtAddVarTab[2],  VarValue << 1) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(EE_ReadVariable(VirtAddVarTab[2],  &VarDataTab[2]) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if ((VarValue << 1) != VarDataTab[2])
-//    {
-//      Error_Handler();
-//    }
-//  }
+//	/* read the last stored variables data*/
+//	if (EE_ReadVariable(VirtAddVarTab[0], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[0]) {
+//		Error_Handler();
+//	}
 //
-//  /* Store 0x2000 values of Variable2 in EEPROM */
-//  for (VarValue = 1; VarValue <= 0x2; VarValue++)
-//  {
-//    if(EE_WriteVariable(VirtAddVarTab[1], VarValue) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(EE_ReadVariable(VirtAddVarTab[1], &VarDataTab[1]) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(VarValue != VarDataTab[1])
-//    {
-//      Error_Handler();
-//    }
-//  }
+//	if (EE_ReadVariable(VirtAddVarTab[1], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[1]) {
+//		Error_Handler();
+//	}
 //
-//  /* read the last stored variables data*/
-//  if(EE_ReadVariable(VirtAddVarTab[0], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[0])
-//  {
-//    Error_Handler();
-//  }
-//  if(EE_ReadVariable(VirtAddVarTab[1], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[1])
-//  {
-//    Error_Handler();
-//  }
+//	if (EE_ReadVariable(VirtAddVarTab[2], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[2]) {
+//		Error_Handler();
+//	}
 //
-//  if(EE_ReadVariable(VirtAddVarTab[2], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[2])
-//  {
-//    Error_Handler();
-//  }
+//	/* Store 0x3000 values of Variable3 in EEPROM */
+//	for (VarValue = 1; VarValue <= 0x3; VarValue++) {
+//		if (EE_WriteVariable(VirtAddVarTab[2], VarValue) != HAL_OK) {
+//			Error_Handler();
+//		}
+//		if (EE_ReadVariable(VirtAddVarTab[2], &VarDataTab[2]) != HAL_OK) {
+//			Error_Handler();
+//		}
+//		if (VarValue != VarDataTab[2]) {
+//			Error_Handler();
+//		}
+//	}
 //
-//  /* Store 0x3000 values of Variable3 in EEPROM */
-//  for (VarValue = 1; VarValue <= 0x3; VarValue++)
-//  {
-//    if(EE_WriteVariable(VirtAddVarTab[2], VarValue) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(EE_ReadVariable(VirtAddVarTab[2], &VarDataTab[2]) != HAL_OK)
-//    {
-//      Error_Handler();
-//    }
-//    if(VarValue != VarDataTab[2])
-//    {
-//      Error_Handler();
-//    }
-//  }
-
-//  /* read the last stored variables data*/
-//  if(EE_ReadVariable(VirtAddVarTab[0], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[0])
-//  {
-//    Error_Handler();
-//  }
+//	/* read the last stored variables data*/
+//	if (EE_ReadVariable(VirtAddVarTab[0], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[0]) {
+//		Error_Handler();
+//	}
 //
-//  if(EE_ReadVariable(VirtAddVarTab[1], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[1])
-//  {
-//    Error_Handler();
-//  }
+//	if (EE_ReadVariable(VirtAddVarTab[1], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[1]) {
+//		Error_Handler();
+//	}
 //
-//  if(EE_ReadVariable(VirtAddVarTab[2], &VarDataTmp) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  if (VarDataTmp != VarDataTab[2])
-//  {
-//    Error_Handler();
-//  }
+//	if (EE_ReadVariable(VirtAddVarTab[2], &VarDataTmp) != HAL_OK) {
+//		Error_Handler();
+//	}
+//	if (VarDataTmp != VarDataTab[2]) {
+//		Error_Handler();
+//	}
 
+	/* USER CODE END 2 */
 
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
 
-
-
-
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin,1);
-	  	HAL_Delay(500);
-  }
-  /* USER CODE END 3 */
+		/* USER CODE BEGIN 3 */
+		HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+		HAL_Delay(500);
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 64;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 64;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, LED_GREEN_Pin|LED_RED_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOD, LED_GREEN_Pin | LED_RED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_GREEN_Pin LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_GREEN_Pin|LED_RED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+	/*Configure GPIO pins : LED_GREEN_Pin LED_RED_Pin */
+	GPIO_InitStruct.Pin = LED_GREEN_Pin | LED_RED_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 }
 
@@ -326,22 +287,20 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-    /* Toggle LED fast */
-	HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-	HAL_Delay(100);
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+		/* Toggle LED fast */
+		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+		HAL_Delay(100);
 
-  }
-  /* USER CODE END Error_Handler_Debug */
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
